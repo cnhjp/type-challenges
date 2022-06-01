986ffd2882b362bdba6566c926cfd1177cc15c49
cnhjp <1304272317@qq.com>
2022-06-01 22:03:43 +0800

    feat: add some solutions

--- questions/00009-medium-deep-readonly/template.ts
+++ questions/00009-medium-deep-readonly/template.ts
@@ -1,1 +1,7 @@
-type DeepReadonly<T> = any
+type DeepReadonly<T> = T extends (...args: any[]) => any
+  ? T
+  : T extends Record<keyof any, any>
+  ? {
+      readonly [K in keyof T]: DeepReadonly<T[K]>
+    }
+  : T
--- questions/00108-medium-trim/template.ts
+++ questions/00108-medium-trim/template.ts
@@ -1,1 +1,14 @@
-type Trim<S extends string> = any
+type MyTrimLeft<S extends string> = S extends `${
+  | " "
+  | "\t"
+  | "\n"}${infer Rest}`
+  ? MyTrimLeft<Rest>
+  : S
+type MyTrimRight<S extends string> = S extends `${infer Rest}${
+  | " "
+  | "\t"
+  | "\n"}`
+  ? MyTrimRight<Rest>
+  : S
+
+type Trim<S extends string> = MyTrimLeft<MyTrimRight<S>>
