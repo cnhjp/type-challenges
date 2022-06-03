726b6442f605cc92f434765f9d41c5da3d6f26fc
cnhjp <tonyhjp888@gmail.com>
2022-06-03 22:36:20 +0800

    feat: add some solutions

--- questions/05317-medium-lastindexof/template.ts
+++ questions/05317-medium-lastindexof/template.ts
@@ -1,1 +1,13 @@
-type LastIndexOf<T, U> = any
+// 先截取到最右边的U，然后求截取后的数组的长度-1
+
+type MySplit<T extends any[], U> = T extends [...infer Rest, infer Last]
+  ? Equal<Last, U> extends true
+    ? T
+    : MySplit<Rest, U>
+  : []
+
+type LastIndexOf<T extends any[], U> = MySplit<T, U>["length"] extends 0
+  ? -1
+  : MySplit<T, U> extends [...infer Rest, infer _Last]
+  ? Rest["length"]
+  : -1
--- questions/05360-medium-unique/template.ts
+++ questions/05360-medium-unique/template.ts
@@ -1,1 +1,15 @@
-type Unique<T> = any
+// 从右往左，如果发现Rest中include了Last，那么不把这个Last放在结果中
+
+type MyIncludes<T extends any[], U> = T extends [infer First, ...infer Rest]
+  ? Equal<First, U> extends true
+    ? true
+    : MyIncludes<Rest, U>
+  : false
+
+type Unique<T extends any[]> = T extends [...infer Rest, infer Last]
+  ? MyIncludes<Rest, Last> extends true
+    ? Unique<Rest>
+    : [...Unique<Rest>, Last]
+  : []
+
+type Foo = Unique<[string, number, 1, "a", 1, string, 2, "b", 2, number]>
