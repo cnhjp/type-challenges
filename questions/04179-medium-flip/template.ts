12f1513d36bb7bc97e4ed52128c3f1682dd61682
hjp <jp.huang@hongzhengtech.com>
2022-06-07 13:23:23 +0800

    feat：add some solutions

--- questions/04179-medium-flip/template.ts
+++ questions/04179-medium-flip/template.ts
@@ -1,1 +1,12 @@
-type Flip<T> = any
+type KeyType = string | number | symbol
+
+// 这里bool类型要额外处理
+type Flip<T> = {
+  [K in keyof T as T[K] extends true
+    ? "true"
+    : T[K] extends false
+    ? "false"
+    : T[K] extends KeyType
+    ? T[K]
+    : never]: K
+}
